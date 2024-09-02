68dda1d3c79f03b14e5e385133af5474d2ec2078
hozifakhussouna <hdhyfhkhaldhswkhh@gmail.com>
2024-09-02 15:41:21 +0300

    model

---
+++ lib/features/home/data/repos/homeRepo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/failrue/failure.dart';
+import 'package:bookly/features/home/data/Models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class RepoHome {
+ Future<Either<Failrue,List<BookModel>>> fatchBestSellerBooks();
+   Future<List<BookModel>> fatchfeaturdaBooks();
+}
